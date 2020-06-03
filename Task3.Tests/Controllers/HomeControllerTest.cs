02f9cb02dec37fcb94c1e043e230fa03ee97a2d8
enkr1 <enkr99@gmail.com>
2020-06-03 14:05:20 +0800

    Add project files.

---
+++ Task3.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task3;
+using Task3.Controllers;
+
+namespace Task3.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
