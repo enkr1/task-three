90e09777b7ca622dd04aae40b746579671209603
enkr1 <enkr99@gmail.com>
2020-06-13 14:16:10 +0800

    reCAPTCHA

---
+++ Task3/Models/ReCAPTCHASettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+//Learned from https://www.youtube.com/watch?v=tdNoArSjIyY
+namespace Task3.Models
+{
+    public class ReCAPTCHASettings
+    {
+        public string reCAPTCHA_Site_Key { get; set; }
+
+        public string reCAPTCHA_Secret_Key { get; set; }
+    }
+}
